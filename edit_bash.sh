d12143a66f816d9ee2166a6cf0584f9dd10a3f3f
Seongho <seonghohan0012@gmail.com>
2024-05-01 18:52:47 +0900

    Added bash edit script.

---
+++ edit_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo '
+# chris was here
+alias eb="gedit ~/.bashrc"
+alias sb="source ~/.bashrc"
+
+# additional routines for tusk
+alias tusc_ready="cd ~/TUSC/BLDC_Test/pigpio-master && sudo ./pigpiod && cd .."
+alias tusc_test="sudo python3 joystick_control_test.py"
+alias tusc_calibrate="sudo python3 calibrate.py"
+alias tusc="tusc_ready && tusc_test"' >> ~/.bashrc
+
