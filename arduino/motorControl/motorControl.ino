97ed8e156073a2f003c19bef1b04cee37af33197
Seongho Han <seonghohan0012@gmail.com>
2024-05-11 18:23:16 +0900

    Updated pins

--- arduino/motorControl/motorControl.ino
+++ arduino/motorControl/motorControl.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
 
-int ESCPin_L = 9;
-//int ESCPin_R = 10;  /* TODO: Find different PWM out pin */
-int inputPin_L = 10;
-//int inputPin_R = 10;  /* TODO: Find different PWM in pin */
+int ESCPin_L = 5;
+int ESCPin_R = 6;  /* TODO: Find different PWM out pin */
+int inputPin_L = 9;
+int inputPin_R = 10;  /* TODO: Find different PWM in pin */
 
 int inputValue_L = 0;
 int inputValue_R = 0;
@@ -26,29 +26,32 @@ void setup()
   delay(500);
 
   ESC_L.attach(ESCPin_L, MIN_PULSEWIDTH, MAX_PULSEWIDTH);
-  //ESC_R.attach(ESCPin_R, MIN_PULSEWIDTH, MAX_PULSEWIDTH);
+  ESC_R.attach(ESCPin_R, MIN_PULSEWIDTH, MAX_PULSEWIDTH);
   pinMode(inputPin_L, INPUT);
-  //pinMode(inputPin_R, INPUT);
+  pinMode(inputPin_R, INPUT);
 }
 
 void loop()
 {
+  /*
   static int currentTime = 0;
-  Serial.println(millis() - currentTime);
-  currentTime = millis();
+  Serial.print("Time interval: ");
+  Serial.print(millis() - currentTime);
+  Serial.println(" ms");
+  currentTime = millis();*/
 
   // Read PWM signals sent from Pi
   int pulseDuration_L = pulseIn(inputPin_L, HIGH);  // Measures duration of HIGH signal
-  //int pulseDuration_R = pulseIn(inputPin_R, HIGH);
+  int pulseDuration_R = pulseIn(inputPin_R, HIGH);
 
   // Set PWM output for ESCs
   int servoValue_L = map(pulseDuration_L, MIN_PULSEWIDTH, MAX_PULSEWIDTH, 0, 180);
-  //int servoValue_R = map(pulseDuration_R, MIN_PULSEWIDTH, MAX_PULSEWIDTH, 0, 180);
+  int servoValue_R = map(pulseDuration_R, MIN_PULSEWIDTH, MAX_PULSEWIDTH, 0, 180);
 
   ESC_L.write(servoValue_L);
-  //ESC_R.write(servoValue_R);
+  ESC_R.write(servoValue_R);
   
-  //printValues(pulseDuration_L, pulseDuration_R);
+  printValues(pulseDuration_L, pulseDuration_R);
 }
 
 
