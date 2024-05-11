f2686e7f38abfad4d43f818188d2d6513589022d
Seongho Han <seonghohan0012@gmail.com>
2024-05-11 18:23:36 +0900

    Changed directories to absolute

--- edit_bash.sh
+++ edit_bash.sh
@@ -7,10 +7,10 @@ alias eb="gedit ~/.bashrc"
 alias sb="source ~/.bashrc"
 
 # additional routines for tusk
-alias tusc_ready="cd ~/TUSC/BLDC_Test/pigpio-master && sudo ./pigpiod && cd .."
-alias tusc_test="sudo python3 joystick_control_test.py"
-alias tusc_calibrate="sudo python3 calibrate.py"
-alias tusc="tusc_ready && tusc_test"
+alias tusc_ready="cd ~/TUSC/pigpio-master && sudo ./pigpiod && cd .."
+alias tusc_run="sudo python3 ~/TUSC/run.py"
+alias tusc_calibrate="sudo python3 ~/TUSC/_legacy/calibrate.py"
+alias tusc="tusc_ready && tusc_run"
 
 ### END OF EDIT ###
 
