26b9e696bc7a054b6ea7906d0d37e4a6f67cd855
Seongho Han <seonghohan0012@gmail.com>
2024-05-25 22:37:27 +0900

    Reads and prints timestamp

--- plot.py
+++ plot.py
@@ -2,7 +2,8 @@ N_DOTS = 80
 MAX_SPEED = 5000  # rpm
 N_HEADER = 3
 
-def print_response(target_value, current_value, max_value, n):
+def print_response(timestamp, target_value, current_value, max_value, n):
+    print(timestamp, end=' ')
     target = int(n * target_value / max_value)
     current = int(n * current_value / max_value)
     if target <= current:
@@ -27,6 +28,7 @@ Lines = file.readlines()
 for i, line in enumerate(Lines):
     if i >= 3:
         line_list = line.strip().split()
-        target_speed = float(line_list[2])
-        current_speed = float(line_list[3])
-        print_response(target_speed, current_speed, MAX_SPEED, N_DOTS)
+        timestamp = int(line_list[0])
+        target_speed = float(line_list[3])
+        current_speed = float(line_list[4])
+        print_response(timestamp, target_speed, current_speed, MAX_SPEED, N_DOTS)
