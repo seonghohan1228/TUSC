2dd4893474f764dfe3cbb065bed8984d8642ed7d
Seongho Han <seonghohan0012@gmail.com>
2024-05-25 22:35:16 +0900

    Added timestamp to output

--- run.py
+++ run.py
@@ -320,6 +320,7 @@ def main():
 
 			if ser.in_waiting > 0:
 				line = ser.readline().decode('utf-8').rstrip()
+				print(time.time_ns(), end=' ')
 				print(line)
 
             # Handle Pygame events
