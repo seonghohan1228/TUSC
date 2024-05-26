d0ba73ce1d95c01ae2707453219b52b7b19fea95
Seongho Han <seonghohan0012@gmail.com>
2024-05-26 12:17:49 +0900

    Added calibration code

--- calibrate.py
+++ calibrate.py
@@ -5,17 +5,14 @@ import pygame
 PWM_PIN_L = 13
 PWM_PIN_R = 12
 MIN_PULSEWIDTH = 1000
-CENTER_PULSEWIDTH = 1500
+IDLE_PULSEWIDTH = 1500
 MAX_PULSEWIDTH = 2000
 
 
 def map_joystick(input):
-	if input > 0:
-		input /= 1
+	pulsewidth = IDLE_PULSEWIDTH + input * 500
 	
-	pulsewidth = CENTER_PULSEWIDTH + input * 500
-	
-	return int(pulsewidth)
+	return pulsewidth
 
 
 def main():
@@ -44,21 +41,24 @@ def main():
                 axis_value_L = -(joystick.get_axis(1))
                 axis_value_R = -(joystick.get_axis(3))
                 
-                speed_L = map_joystick(axis_value_L)
-                speed_R = map_joystick(axis_value_R)
+                pulsewidth_L = map_joystick(axis_value_L)
+                pulsewidth_R = map_joystick(axis_value_R)
 
-                print(f"Speed (L): {speed_L}\tSpeed (R): {speed_R}")
-                #print(f"Right motor: {axis_value_R:1.3}")
+                print(f"Pulsewidth (L): {pulsewidth_L:4.2f}\Pulsewidth (R): {pulsewidth_R:4.2f}")
                 
-                pi.set_servo_pulsewidth(PWM_PIN_L, speed_L)
-                pi.set_servo_pulsewidth(PWM_PIN_R, speed_R)
+                pi.set_servo_pulsewidth(PWM_PIN_L, pulsewidth_L)
+                pi.set_servo_pulsewidth(PWM_PIN_R, pulsewidth_L)
     
     except KeyboardInterrupt:
         print("Keyboard interrupt")
+        pi.set_servo_pulsewidth(PWM_PIN_L, IDLE_PULSEWIDTH)
+        pi.set_servo_pulsewidth(PWM_PIN_R, IDLE_PULSEWIDTH)
         pi.stop()
         pygame.quit()
 
     finally:
+        pi.set_servo_pulsewidth(PWM_PIN_L, IDLE_PULSEWIDTH)
+        pi.set_servo_pulsewidth(PWM_PIN_R, IDLE_PULSEWIDTH)
         pi.stop()
 
 
